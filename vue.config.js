99f71baeeff8184467113d8a89414f7668670e31
Reiya <reiya0104@ezweb.ne.jp>
2021-11-18 02:13:24 +0900

    Update outputDir

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-test-app/'   //任意
-    : '/'
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/vue-test-app/'   //任意
+        : '/',
+    outputDir: 'dist'
 }
