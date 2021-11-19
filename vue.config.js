28613c4d3f212c1e012c8bbc1eb64ba1027835ab
Reiya <reiya0104@ezweb.ne.jp>
2021-11-19 20:56:24 +0900

    Delete project

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   outputDir: 'docs',
   assetsDir: './',
-  publicPath: './',
-  pages: {
-    index: {
-      entry: 'src/main.js',
-      title: 'vue-test',
-    },
-  },
+  publicPath: './'
 };
