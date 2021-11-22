28e66522af7a7037fda79e0b8c297364768d139d
Reiya <reiya0104@ezweb.ne.jp>
2021-11-22 17:40:18 +0900

    Build project

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   outputDir: 'docs',
-  assetsDir: './',
-  publicPath: './',
+  // assetsDir: './',
+  publicPath: '/vue-test',
+  filenameHashing: false,
+  productionSourceMap: false,
 }
